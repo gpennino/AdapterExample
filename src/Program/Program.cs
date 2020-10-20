31a405d7e367d8197e5714470fe4ebbeaedeb05e
gpennino <gpennino@adinet.com.uy>
2020-10-20 18:19:46 -0300

    Initial commit

---
+++ src/Program/Program.cs
@@ -0,0 +1,28 @@
+﻿//-------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Universidad Católica del Uruguay">
+//     Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//-------------------------------------------------------------------------
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace AdapterExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ISmartDevice sd1 = new Bulb("ID01");
+            ISmartDevice wm1 = new WashingMachine("ID02");            
+
+  
+
+
+
+
+
+        }
+    }
+}
